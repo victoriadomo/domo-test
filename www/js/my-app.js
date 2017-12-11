322a63c1796fb4eb149b483016e90587ec3f6eb9
victoriadomo <victoria@domo.com.ar>
2017-12-11 12:52:26 -0300

    inicio

---
+++ www/js/my-app.js
@@ -0,0 +1,77 @@
+// Initialize app
+var myApp = new Framework7();
+
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    console.log("Device is ready!");
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+myApp.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+// $$(document).on('pageInit', function (e) {
+//     // Get page data from event data
+//     var page = e.detail.page;
+
+//     if (page.name === 'about') {
+//         // Following code will be executed for page with data-page attribute equal to "about"
+//         myApp.alert('Here comes About page');
+//     }
+// })
+
+// // Option 2. Using live 'pageInit' event handlers for each page
+// $$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+//     // Following code will be executed for page with data-page attribute equal to "about"
+//     myApp.alert('Here comes About page');
+// })
+
+/* FEEDS  */
+
+// var feed = "http://clientes.domo.com.ar/DOMO/web/www_alfa/wordpress/feed/";
+// ajax(feed, {
+//   dataType: "xml",
+//   success: cargarNoticias
+// });
+
+
+var feed = "http://clientes.domo.com.ar/DOMO/web/www_alfa/wordpress/feed/";
+$.ajax(feed, {
+  dataType: "text",
+  success: traerRSS
+});
+
+
+function traerRSS(data) {
+  var maximo = 5;
+  var items = $(data).find("item");
+  
+  for (var index=0; index < items.length && index <= maximo; index++) {
+  
+         var el = $(items[index]);
+         var imagen = el.find("imagen").text();     
+         var titulo = el.find("title").text();    
+         var descripcion = el.find("contenido").text();
+         var tipoPost = el.find("tipopost").text();
+         console.log(tipoPost);
+
+         $("#traer").append(descripcion);
+  }
+  
+}
